51626e366971100e867d53db27e13b036437e50f
vivek mishra <vm368@afsconnect1.njit.edu>
2017-11-25 14:03:30 -0500

    collection class as abstarct

---
+++ collection.php
@@ -0,0 +1,33 @@
+<?php
+// collection as abstract 
+//namespace pdo\coll;
+abstract class collection {
+    static public function create() {
+        $model = new static::$modelName;
+        return $model;
+    }
+    static public function findAll() {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        echo " Print full table:" .$tableName;
+		return $recordsSet;
+    }
+    static public function findOne($id) {
+        $db = dbConn::getConnection();
+        $tableName = get_called_class();
+        $sql = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $class = static::$modelName;
+        $statement->setFetchMode(PDO::FETCH_CLASS, $class);
+        $recordsSet =  $statement->fetchAll();
+        echo "Print one record :". $tableName;
+		return $recordsSet[0];
+    }
+}
